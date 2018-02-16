242f5cf36a4969bc5b961278c40fb4047a832919
Carl <fonts@spoturdeal.com>
2018-02-16 13:53:33 +0100

    Changed to a better graphic layout

--- MMM-rain-forecast.js
+++ MMM-rain-forecast.js
@@ -6,21 +6,13 @@
  *  https://br-gpsgadget-new.azurewebsites.net/data/raintext?lat=52.15&lon=5.5
  * By Spoturdeal.
  */
-
 Module.register("MMM-rain-forecast",{
 	// Default module config.
 	defaults: {
 		lat: 52.15,
         lon: 5.5,
-        updateInterval: 10 * 60 * 1000, // every 10 minutes
-		animationSpeed: 1000,
-		lang: config.language,
-		initialLoadDelay: 0, // 0 seconds delay
-		lineColor: "#0074d9",
-        fillColor: "#0074d9",		
-		width: 200,
-		height: 150,
-		showConsumption: false
+		noRainText: 'Until %s no rain',    // write %s where you like to add the time example Until 17:15 no rain
+        pleaseWait: 'Please wait'
 	},
     // Override start method.
 	start: function() {
@@ -35,110 +27,106 @@ Module.register("MMM-rain-forecast",{
 		});
 
 	},
-	// Define required scripts.
+	// Define required scripts. No extra scripts needed
 	getScripts: function() {
 		return [];
 	},
-
-	// Define required scripts.
+	// Define required styles for chart only.
 	getStyles: function() {
 		return ["MMM-rain-forecast.css"];
 	},
 	socketNotificationReceived: function(notification, payload) {
-    	
+        // was not able to receive data 
         if (notification == "ERROR") {
 			document.getElementById("sparkler").innerHTML=payload.error;
 			return;
 		}
-	    var numberTimeLabels = this.config.nrOfTimeLabels ? this.config.nrOfTimeLabels : 0;
-			
+        // no data received from node_helper.js
         if (!payload.times || payload.times.length == 0) {
             document.getElementById("sparkler").innerHTML="No Data";
-            this.emptyCols(numberTimeLabels);
             return;
         }
+        // no rain calculated from in node_helper.js 
         if (payload.expectRain == 0) {
-            // no rain calculated from data 
-            noRainText = this.config.noRainText ? this.config.noRainText : "No rain until: ";
-            document.getElementById("sparkler").innerHTML=noRainText + payload.times[payload.times.length-1];
-            this.emptyCols(numberTimeLabels);
+            noRainText = this.sprintf(this.config.noRainText,payload.times[payload.times.length-1]);
+            document.getElementById("sparkler").innerHTML=noRainText 
         } else {
-            if (numberTimeLabels>0) {
-                interval = Math.floor(payload.times.length / (numberTimeLabels-1));
-                for (i = 0; i < numberTimeLabels; i++) {
-                    if (i==0) { document.getElementById("labelcol0").innerHTML = payload.times[0]; }
-                    else if (i+1==numberTimeLabels) { document.getElementById("labelcol"+i).innerHTML= payload.times[payload.times.length-1]; }
-                    else { document.getElementById("labelcol"+i).innerHTML = payload.times[i*interval]; }
-                }
-            } else {
-                this.emptyCols(numberTimeLabels);
-                rainText = this.config.rainText ? this.config.rainText : "Forecast until: ";
-                document.getElementById("textrow").unnerHTML = rainText + payload.times[payload.times.length-1];
-            }
-            
-            document.getElementById('sparkler').innerHTML = this.makeSVG(payload.rainDrops)
+            document.getElementById('sparkler').innerHTML = this.makeSVG(payload.rainDrops,payload.times)
         }
     },
 	// Override dom generator.
 	getDom: function() {
 		var wrapper = document.createElement("table");
 		wrapper.align = "center";
-		wrapper.style.cssText = "width: " + this.config.width + "px";
-		var numberTimeLabels = this.config.nrOfTimeLabels ? this.config.nrOfTimeLabels : 0;
-
-		if (numberTimeLabels==0) {
-			var currentRow = document.createElement("tr");
-			var textrow = document.createElement("td");
-			textrow.className = "normal rfc_textrow";
-			textrow.setAttribute("colspan", numberTimeLabels)
-			textrow.id = "textrow";
-			currentRow.appendChild(textrow);
-			wrapper.appendChild(currentRow);
-		}
-
-		var graphRow = document.createElement("tr");
-		var graph = document.createElement("td");
-		graph.colSpan = numberTimeLabels;
-        graph.id = "sparkler";
-		graph.className = "small thin light";
-		graph.innerHTML = "Een ogenblik a.u.b.";
-		graphRow.appendChild(graph);
-		wrapper.appendChild(graphRow);
-
-		if (numberTimeLabels>0) {
-			var botRow = document.createElement("tr");
-			for (i = 0; i < numberTimeLabels; i++) {
-				var labelcol = document.createElement("td");
-				labelcol.className = "xsmall thin light rfc_labelrow";
-				labelcol.id = "labelcol"+i;
-				
-				if (i==0) { labelcol.style.textAlign="left"; }
-				else if (i+1==numberTimeLabels) { labelcol.style.textAlign="right"; }
-				else { labelcol.style.textAlign="center"; }
-				
-				botRow.appendChild(labelcol);
-			}
-			wrapper.appendChild(botRow);
-		}
-
+		var graphTr = document.createElement("tr");
+		var graphTd = document.createElement("td");
+        graphTd.id = "sparkler";
+		graphTd.className = "small thin light";
+		graphTd.innerHTML = this.config.pleaseWait;
+		graphTr.appendChild(graphTd);
+		wrapper.appendChild(graphTr);
 		return wrapper;
 	},
-    emptyCols: function(cnt){
-        for (i = 0; i < cnt; i++) {
-           document.getElementById("labelcol"+i).innerHTML="";
-        }
-        return;
-    },
-    makeSVG: function(raining){
-        var setPoints='0,220';
+    // Make the graphic using SVG
+    makeSVG: function(raining,times){
+        /* We start at position
+         * The table is upside down therefor we calculate the line position down from the top of the canvas
+         * received value 77 = 200 - 77 = 123 on the canvas 
+         * M01,200 is the start
+         */
+        var setPoints='M01,200';
+        // loop through the received data array raining[] normally 24 position 0 to 23
         var xAs=1;
         for (i=0;i<raining.length;i++){
             xAs=(xAs==1?xAs=2:xAs+20);         
-            setPoints += ' ' + xAs + ',' + (220-raining[i]); 
+            setPoints += ', L' + xAs + ',' + (200-raining[i]); 
         }
-        xAs++;
-        setPoints +=' ' + xAs + ',220';
-        var svg='<svg viewBox="0 0 400 100" class="chart"> <polyline fill="'+this.config.fillColor+'" stroke="'+this.config.lineColor+'" stroke-width="2" points="' + setPoints + '"   />';
+        // End of th3 line make sure it drops to the bottom of the canvas to avoid silly fill
+        setPoints +=', L' + xAs + ',200 Z';
+        var svg='<svg class="graph" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">';
+        //Set grid lines xAs ans yAs size is determined in CSS
+        svg+='<g class="grid x-grid" id="xGrid"><line x1="1" x2="1" y1="00" y2="200"></line></g>';
+        svg+='<g class="grid y-grid" id="yGrid"><line x1="1" x2="400" y1="200" y2="200"></line></g>';
+        //Draw the line with the data 
+        svg+='<g class="surfaces">';
+        svg+='<path class="first_set" d="' + setPoints + '"></path>';
+        svg+='</g>';
+        // Set the class for the grid
+        svg+='<use class="grid double" xlink:href="#xGrid" style=""></use><use class="grid double" xlink:href="#yGrid" style=""></use>';
+        // Time labels
+        svg+='<g class="labels x-labels">';
+        svg+='<text x="20" y="215"  fill="white">' + times[1] + '</text>';
+        svg+='<text x="87" y="215"  fill="white">' + times[5]+ '</text>';
+        svg+='<text x="154" y="215" fill="white">' + times[9]+ '</text>';
+        svg+='<text x="221" y="215" fill="white">' + times[13] + '</text>';
+        svg+='<text x="288" y="215" fill="white">' + times[17] + '</text>';
+        svg+='<text x="355" y="215" fill="white">' + times[21] + '</text>';
+        svg+='</g></svg>';
         return svg;
-    }
+    },
+    sprintf: function() {
+    var args = arguments,
+    string = args[0],
+    i = 1;
+    return string.replace(/%((%)|s|d)/g, function (m) {
+        // m is the matched format, e.g. %s, %d
+        var val = null;
+        if (m[2]) {
+            val = m[2];
+        } else {
+            val = args[i];
+            // A switch statement so that the formatter can be extended. Default is %s
+            switch (m) {
+                case '%d':
+                    val = parseFloat(val);
+                    if (isNaN(val)) {
+                        val = 0;
+                    }
+                    break;
+            }
+            i++;
+        }
+        return val;
+    });
+}
 });
--- node_helper.js
+++ node_helper.js
@@ -19,37 +19,45 @@ module.exports = NodeHelper.create({
 		return;
 	},
 	getData: function(self) {
-        // options = {url: self.url,, method: 'GET'}
  		request({url: self.url, method: 'GET'}, function(error, response, body) {
 			self.processData(error, response, body, self);
 		});
 		
 	},
     processData: function(error, response, body, self) {
+       // First handle server side errors
        if (error) {
 			self.sendSocketNotification("ERROR", {
 				error: "Error ",
 			});
 			return;
 		}
+        // Page or url has not been found
 		if (response.statusCode != 200) {
 			self.sendSocketNotification("ERROR", {
 				error: body,
 			});
 			return;
 		}
+        // This is test data just to see the graph if there is no rain
+        //body="077|10:05\n034|10:10\n101|10:15\n087|10:20\n077|10:25\n000|10:30\n000|10:35\n000|10:40\n077|10:45\n087|10:50\n087|10:55\n077|11:00\n077|11:05\n034|11:10\n017|11:15\n000|11:20\n000|11:25\n000|11:30\n000|11:35\n000|11:40\n000|11:45\n000|11:50\n000|11:55\n000|12:00";
+        
+        // Make an array with the amount of rain  077|10:05 = rain|time
         var rainDrops = [];
+        // Make an array with the times received
 		var times = [];
+        // Count all rain together
 		var expectRain = 0;
-        //console.log(body);
+        // Make seprate lines
 		var lines = body.split('\n');
 		for(var i = 0;i < lines.length-1;i++){
 			var values = lines[i].split('|');
-			
+            // split rain from time
             rainDrops.push(values[0]=="NaN"?0:parseInt(values[0]));
 			times.push(values[1]);
 			expectRain += parseInt(values[0]);
 		}
+        // Send all to script
         self.sendSocketNotification('RAIN_DATA', {
             rainDrops:  rainDrops,
             times:      times,
